8706197b0e8a0c80b0ae4d711af59de8ce46382b
stravo1@mac <stravoone@gmail.com>
2026-05-06 19:15:09 +0530

    fix: delete outdated page directories
    previous method of finding dir names was wrong as `page` only contained `name` not the entire details

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -3,27 +3,30 @@ import FrappeClient from "../lib/frappeClient";
 import writePage from "../lib/writePage";
 import { deleteDir, readDir, readFile, fileExists } from "../utils/file";
 import { logger } from "../utils/logger";
-import { safeFileName } from "../utils/misc";
+import path from "path";
 
 const CONFIG_FILE = "config.json";
 
 export const pull = async (client: FrappeClient) => {
     try {
         const pages = await client.getPages();
+        const dirList = [];
         for (const page of pages) {
-            writePage(client, page); // Replace "output" with your desired output directory
+            const dirName = await writePage(client, page);
+            if (dirName) {
+                dirList.push(dirName);
+            }
         }
+
         // delete pageDirs which are not present in the pages list
-        // get list of pageDirs in the output directory
-        const path = await import("path");
         const outputDir = path.join(process.cwd(), "pages");
         if (fileExists(outputDir)) {
             const pageDirs = readDir(outputDir);
             for (const pageDir of pageDirs) {
-                if (!pages.some((page: any) => safeFileName(`${page.name || page.element || "unnamed"}_${page.pageId}`) === pageDir)) {
+                if (!dirList.some((dirName: string) => dirName === pageDir)) {
                     deleteDir(path.join(outputDir, pageDir));
                     logger.info(
-                        `Deleted local directory for removed page: ${pageDir}`,
+                        `Deleted local directory for removed page: ${path.join(outputDir, pageDir)}`,
                     );
                 }
             }
